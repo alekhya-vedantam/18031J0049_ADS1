abf1680681ba969dd3f2b179d7635e61200ce845
alekhya-vedantam <vrsalekya@gmail.com>
2018-10-23 15:50:59 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

---
+++ ADS-1/m8/m_2/8.1 Team Ranking With Insertion/InsertionSort1.java
@@ -0,0 +1,73 @@
+package sortings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class Teams implements Comparable<Teams> {
+	String Tname;
+	int wins;
+	int losses;
+	int draws;
+
+	Teams(String Tname, int wins, int losses, int draws) {
+		this.Tname = Tname;
+		this.wins = wins;
+		this.losses = losses;
+		this.draws = draws;
+	}
+
+	@Override
+	public int compareTo(Teams Ts) {
+		if (this.wins == Ts.wins) {
+			if (this.losses == Ts.losses) {
+				return Ts.draws - this.draws;
+			} else {
+				return this.losses - Ts.losses;
+			}
+		} else {
+			return Ts.wins - this.wins;
+		}
+
+	}
+}
+
+public class InsertionSort1 {
+	public static void main(String args[]) {
+
+		ArrayList<Teams> al = new ArrayList<>();
+		Scanner scn = new Scanner(System.in);
+		do {
+			String st = scn.nextLine();
+			String sp[] = st.split(",");
+			al.add(new Teams(sp[0], Integer.parseInt(sp[1]), Integer.parseInt(sp[2]), Integer.parseInt(sp[3])));
+			for (int i = 1; i < al.size(); i++) {
+				for (int j = i; j > 0; j--) {
+					if (al.get(j - 1).wins < al.get(j).wins) 
+						al.add(j, al.remove(j - 1));
+					else if(al.get(j - 1).wins == al.get(j).wins) {
+						if (al.get(j - 1).losses > al.get(j).losses)
+							al.add(j, al.remove(j - 1));
+						else if(al.get(j - 1).losses == al.get(j).losses) {
+							al.add(j, al.remove(j - 1));
+						}
+					} else 
+						break;
+					
+				}
+			}
+			
+		} while (scn.hasNext());
+		Collections.sort(al);
+		// System.out.println(al);
+		String st = "" + al.get(0).Tname;
+		for (int i = 1; i < al.size(); i++) {
+			st = st + "," + al.get(i).Tname;
+
+		}
+		System.out.print(st);
+		// System.out.print(ts.Tname+",");
+	}
+}
